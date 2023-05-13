977ed63c4849d452cbfd0e4d7f0f6630d32e5dbb
linapatel21 <128197500+linapatel21@users.noreply.github.com>
2023-05-13 18:25:17 +0000

    final

--- main.tf
+++ main.tf
@@ -2,9 +2,12 @@ resource "aws_instance" "cornerstone" {
   ami           = "ami-02396cdd13e9a1257" # us-west-1
   instance_type = "t2.micro"
   user_data = file("docker.sh")
-  security_groups = ["default"]
-}
+  security_groups = ["launch-wizard-2"]
 
+ tags = {
+    "Name" = "CS-Docker"
+  }
+}
 
 
 
